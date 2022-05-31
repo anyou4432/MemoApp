6008b1488fab3d91e235ae4fd276658807a3b81e
anyou4432 <y_a4432@yahoo.co.jp>
2022-06-01 00:03:34 +0900

    Firebase login and signup

--- Udemy/MemoApp/App.jsx
+++ Udemy/MemoApp/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -9,7 +10,13 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 
+import { firebaseConfig } from './env';
+
 const Stack = createStackNavigator();
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
@@ -18,7 +25,7 @@ export default function App() {
         initialRouteName="SignUp"
         screenOptions={{
           headerStyle: { backgroundColor: '#467FD3' },
-          headerTitleStyle: {color: '#ffffff'},
+          headerTitleStyle: { color: '#ffffff' },
           headerTitle: 'Memo App',
           headerTintColor: '#ffffff',
           headerBackTitle: 'Back',
